a711eed907050e334b5aa96b758d032230e5ad31
Bartosz <grypsy@gmail.com>
2017-08-14 08:20:01 +0000

    many changes

--- jquery.popup.js
+++ jquery.popup.js
@@ -1,56 +1,65 @@
 /* Initialization of user variables needed by the plugin */
 var fadeOutSpeed = 200;
-var fadeInSpeed = 300;
-var imageHeight = "78%";
+var fadeInSpeed = 200;
+var imageSize = "92%";
 var captions = false;
 
 /*
 closing popup function
 */
 function closePopup() {
-        $( "#photo" ).fadeOut(fadeOutSpeed,function() {
-            $( "#popup" ).fadeOut(fadeOutSpeed, function() {
-                $( "#photo" ).attr('src',"#");
+        $( "#photo" ).fadeOut(fadeOutSpeed,function() {			// fadeout the photo
+            $( "#popup" ).fadeOut(fadeOutSpeed, function() {	// fadeout the the popup 
+                $( "#photo" ).attr('src',"#");					// set the src for the photo to #
             });
         });
-		$( ".wrap" ).show();	// show hidden main content to make scrollbar enabled
+		$( ".wrap" ).show();									// show hidden main content to make scrollbar enabled
 }
 
 /*
 document ready main function
 */
 $( document ).ready(function() {
-    /*$( "#close" ).click(function() {
-        $( "#photo" ).fadeOut(200,function() {
-            $( "#popup" ).fadeOut(200, function() {
-                $( "#photo" ).attr('src',"#");
-            });
-        });
-    });
-	$( "#popup" ).click(function() {
-        $( "#photo" ).fadeOut(200,function() {
-            $( "#popup" ).fadeOut(200, function() {
-                $( "#photo" ).attr('src',"#");
-            });
-        });
-    });
-	*/
+	var i = 0;
+	
+	var tn_array = $(".wrap img").map(function() {
+		return $(this).attr("data-src");
+	});
+	
 	$( "#close" ).click(closePopup);
-	$( "#popup" ).click(closePopup);
 	
     
 	$( ".box" ).click(function() {
         var img = $(this).children().children(img).attr('data-src');
-		$( ".wrap" ).hide();
-        $( "#popup" ).fadeIn(fadeInSpeed);
-		$( "#photo" ).height(imageHeight); //100determinuje weilkosc obrazka
-		//$( "#photo" ).height($( window ).height()-300); //100
-        $( "#photo img" ).attr('src',img);
-        $( "#photo" ).fadeIn(fadeInSpeed);
+		
+		$( ".wrap" ).hide();									// hide the main window to disable scrollbars
+
+		$( "#photo" ).height(imageSize); 						// set photo size before show it		
+		$( "#photo" ).width(imageSize);							// set photo size before show it
+        $( "#photo img" ).attr('src',img);						// set photo src to the current one
+        
+		
+		$( "#popup" ).toggle(0,function() {						// show the popup window
+            $( "#photo" ).fadeIn(fadeInSpeed, function() {		// fadein the photo
+                $( "#photo" ).attr('src',"#");					// set photo src attribute to "#"
+			});
+		});   
     });
 	
-    $( window ).bind("resize", function(){
-		$( "#photo" ).height(imageHeight);
-        //$( "#photo" ).height($( window ).height()-300); //100
-    });
+	
+	
+	/* This function changes the current photo to the next one.
+	 * by clicking on the image
+	 */
+	$( "#photo" ).click(function() {
+		$( "#photo" ).fadeOut(fadeOutSpeed,function() {			// fadeout the current photo
+			i++;
+			var img = tn_array[i];
+			
+			$( "#photo img" ).attr('src',img);					// set the src for new photo 
+			$( "#photo" ).fadeIn(fadeInSpeed);					// fadein the new photo
+		});
+	});
+	
+    
 });
